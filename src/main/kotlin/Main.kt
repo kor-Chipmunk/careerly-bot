74570fb2d2628bf597fb7e310ce7c9807b6f3c1e
kor-Chipmunk <rhj4862@gmail.com>
2024-05-04 10:20:21 +0900

    refactor: 메소드명에 복수형 제거

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -29,7 +29,7 @@ suspend fun sendWebHooks(webHookData: WebHookData) {
 
 fun main() = runBlocking {
     val rssParser = RssParser()
-    val rss = REQUEST_URL.httpGet().body.sanitizeInvalidXMLUnicodes()
+    val rss = REQUEST_URL.httpGet().body.sanitizeInvalidXMLUnicode()
     val rssChannel = rssParser.parse(rss)
 
     val embeds = mapToEmbeds(rssChannel)
@@ -72,7 +72,7 @@ val REQUEST_URL = "https://careerly.co.kr/rss/dev"
 
 val GREEN_COLOR = "38912"
 
-fun String.sanitizeInvalidXMLUnicodes(): String {
+fun String.sanitizeInvalidXMLUnicode(): String {
     val SANITIZE_REGEX = Regex("[^\\x09\\x0A\\x0D\\x20-\\uD7FF\\uE000-\\uFFFD\\u10000-\\u10FFFF]")
     return this.replace(SANITIZE_REGEX,"");
 }
